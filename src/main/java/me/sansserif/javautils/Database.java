bc08676dc7c030294265e815fe6eda56f8e51ab8
SansSerif <34772045+SansSerif1@users.noreply.github.com>
2023-04-06 20:42:01 +0200

    new database class
    simple lightweight connection manager with automatic closing

---
+++ src/main/java/me/sansserif/javautils/Database.java
@@ -0,0 +1,45 @@
+package me.sansserif.javautils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Database implements Runnable {
+    private String url;
+    private Duration timeout;
+    private Thread renewer = new Thread(this);
+    private LocalDateTime candie;
+    private Connection current = null;
+
+    public Database(String ip, int port, String username, String password, String dbname, String jdbc_type, Duration timeout) throws SQLException {
+        this.url = "jdbc:" + jdbc_type + "://" + ip + ":" + port + "/" + dbname + "?user=" + username + "&password=" + password;
+        this.timeout = timeout;
+
+        current = DriverManager.getConnection(url);
+        getConn();
+    }
+    public Connection getConn() throws SQLException {
+        candie = LocalDateTime.now().plus(timeout);
+        if (current.isClosed()) current = DriverManager.getConnection(url);
+        if (!renewer.isAlive()) {
+            if (renewer.getState().equals(Thread.State.TERMINATED))
+                renewer = new Thread(this);
+            renewer.start();
+        }
+        return current;
+    }
+
+    @Override
+    public void run() {
+        while (Duration.between(candie, LocalDateTime.now()).isNegative()) {
+            try {
+                Thread.sleep(Duration.between(LocalDateTime.now(), candie));
+            } catch (Exception ignored) {}
+        }
+        try {
+            current.close();
+        } catch (SQLException ignored) {}
+    }
+}
